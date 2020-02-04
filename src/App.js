07e06beb1c835e6be536cc97c80df9450341e6ea
mohammadameer <mohammadameerabdallah@gmail.com>
2020-02-04 22:07:20 +0200

    setup react router dom

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import React from "react";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
 
 // material ui font
 import "typeface-roboto";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Switch></Switch>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
