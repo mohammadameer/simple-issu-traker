db15ee21058ae42cdddbf5c60bfca6185ab6478b
mohammadameer <mohammadameerabdallah@gmail.com>
2020-02-04 21:50:22 +0200

    setup redux

---
+++ src/store.js
@@ -0,0 +1,19 @@
+import { createStore, applyMiddleware } from "redux";
+import reduxThunk from "redux-thunk";
+import logger from "redux-logger";
+
+// combined reducers
+import reducers from "./reducers/index";
+// create store with middlewares
+const createStoreWithMiddleware = applyMiddleware(
+  logger,
+  reduxThunk
+)(createStore);
+
+//add combined reduces to the store
+const store = createStoreWithMiddleware(
+  reducers,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
+
+export default store;
